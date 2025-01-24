dbf70ad941b53b115d673af2ab46d4bf68de76ac
taedc <github@tedeytan.org>
2025-01-24 11:37:31 -0500

    Updated with important note

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,21 @@
-const path = require('path');
-const postCSSPlugins = [
-    require('postcss-simple-vars'),
-    require('postcss-nested'),
-    require('autoprefixer')
-]
+const path = require("path")
+
+const postCSSPlugins = [require("postcss-simple-vars"), require("postcss-nested"), require("autoprefixer")]
+
 module.exports = {
-    entry: './app/assets/scripts/App.js',
-    output: {
-        filename: 'bundled.js',
-        path: path.resolve(__dirname, 'app')
-    },
-    mode: 'development',
-    watch: true,
-    module: {
-        rules: [
-            {
-                test: /\.css$/i,
-                use: ['style-loader', 'css-loader', {loader: 'postcss-loader', options: {postcssOptions: {plugins: postCSSPlugins}}}]
-            }
-        ]
-    }
+  entry: "./app/assets/scripts/App.js",
+  output: {
+    filename: "bundled.js",
+    path: path.resolve(__dirname, "app")
+  },
+  mode: "development",
+  watch: true,
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", { loader: "css-loader", options: { url: false } }, { loader: "postcss-loader", options: { postcssOptions: { plugins: postCSSPlugins } } }]
+      }
+    ]
+  }
 }
