4d8ae899268fdf9dec01dd32911b7c80bd395bf4
taedc <github@tedeytan.org>
2025-01-30 10:38:44 -0500

    Mobile First Lesson

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,9 @@
+const { dir } = require("console")
 const path = require("path")
 
 const postCSSPlugins = [
   require('postcss-import'),
+  require("postcss-mixins"),
   require("postcss-simple-vars"), require("postcss-nested"), require("autoprefixer")]
 
 module.exports = {
@@ -10,8 +12,16 @@ module.exports = {
     filename: "bundled.js",
     path: path.resolve(__dirname, "app")
   },
+  devServer:{
+    watchFiles: ["app/**/*.html"],
+    static: {
+      directory: path.join(__dirname, 'app'),
+      watch: false
+    },
+    hot: true,
+    port: 3000
+  },
   mode: "development",
-  watch: true,
   module: {
     rules: [
       {
