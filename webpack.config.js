d926a4e5007ef9d5cd0b0f447bd116e56878e613
taedc <github@tedeytan.org>
2025-02-26 10:47:31 -0500

    completed desktop and mobile header styles

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,10 @@ const path = require("path")
 const postCSSPlugins = [
   require('postcss-import'),
   require("postcss-mixins"),
-  require("postcss-simple-vars"), require("postcss-nested"), require("autoprefixer")]
+  require('postcss-hexrgba'),
+  require("postcss-simple-vars"), 
+  require("postcss-nested"), 
+  require("autoprefixer")]
 
 module.exports = {
   entry: "./app/assets/scripts/App.js",
